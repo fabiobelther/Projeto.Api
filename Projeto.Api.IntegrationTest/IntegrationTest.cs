b04f50e82cffc33fce0510605f4f84097aac91c3
Fabio Belther <fabio.belther@gmail.com>
2021-01-18 19:23:50 -0300

    IntegrationTests

---
+++ Projeto.Api.IntegrationTest/IntegrationTest.cs
@@ -0,0 +1,46 @@
+using System.Threading.Tasks;
+using Projeto.Api.Host;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+
+namespace Projeto.Api.IntegrationTest
+{
+    public class GeneralPageTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public GeneralPageTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        public static IEnumerable<object[]> ValidUrls => new List<object[]>
+        {
+            new object[] {"/v1/api1/taxaJuros", "0.01", "application/json"},
+            new object[] {"/v1/api2/calculaJuros?valorinicial=100&tempo=5", "105.1", "application/json"},
+            new object[] {"/v1/api2/showmethecode", "https://github.com/fabiobelther/Projeto.Api", "text/plain" }
+        };
+
+        [Theory]
+        [MemberData(nameof(ValidUrls))]
+        public async Task ValidUrls_ReturnSuccessAndExpectedContentType(string path, string responseExpected, string mediaType)
+        {
+            var expected = new MediaTypeHeaderValue(mediaType);
+
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(path);
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(responseExpected, responseString);
+            Assert.Equal(expected.MediaType, response.Content.Headers.ContentType.MediaType);            
+        }
+
+    }
+}
